b58d28455fa3e02cdb00a349b50645a58015bc04
denhed <dennis.hedlund@me.com>
2013-09-09 10:27:27 +0200

    Blogs klassen är uppdaterad, samt Test.classen.

---
+++ VemBloggen/src/se/vem/databas/BlogsMapper.java
@@ -0,0 +1,116 @@
+package se.vem.databas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import se.vem.data.Blogs;
+
+
+public class BlogsMapper {
+	
+	private static BlogsMapper blogsMapper = new BlogsMapper();
+	private DatabaseConnection connection = null;
+	private static Logger logg = null;
+	
+	private BlogsMapper() {
+		logg = Logger.getLogger("se.vemprojekt.databas");
+		connection = DatabaseConnection.getInstance();
+		logg.info("Singeltobject created");
+	}
+	
+	public static BlogsMapper getInstance() {
+		return blogsMapper;
+	}
+	
+	/**
+	 * Sparar nytt blogs objekt i databasen. (Blogs.class).
+	 * @return retunerar ett Blogs objekt, det som sparades.
+	 */
+	public Blogs createBlog(Blogs blog){
+		EntityManager em = connection.getEntityManager();
+		try {
+			em.getTransaction().begin();
+			em.persist(blog);
+			em.getTransaction().commit();
+		} finally {
+			if(em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}	
+		}
+		em.close();
+		return blog;
+	} 
+	
+	/**
+	 * Redigerar ett Blogs objekt från Blogs-tabellen (Blogs.class).
+	 * @return retunerar det redigerade Blogs objektet.
+	 */
+	public Blogs getBlog(int id){
+		EntityManager em = connection.getEntityManager();
+		Blogs blog = null;
+		
+		em.getTransaction().begin();
+		
+		try {
+			
+			TypedQuery<Blogs> blogQuery = em.createQuery("SELECT blogs FROM Blogs blogs WHERE Blogs.blog_id = :blogId", Blogs.class); 
+			blogQuery.setParameter("blogId", id); //sätter parameter (namn på parametern i queryn, värdet som skickas in).
+			blog  = blogQuery.getSingleResult(); // vi tar emot ett objekt.
+			em.getTransaction();
+		} finally {
+			if(em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+			em.close();
+		}
+		return blog;
+	} 
+	
+	
+	/**
+	 * Hämtar alla Blogs objekt från Blogs-tabellen (Blogs.class).
+	 * @return retunerar alla Blogs objekt.
+	 */
+	public List<Blogs> listAllBlogs() {
+		EntityManager em = connection.getEntityManager();
+		List<Blogs> allBlogs = new ArrayList<Blogs>();
+		
+		em.getTransaction().begin();
+		
+		try {
+			TypedQuery<Blogs> b = em.createQuery("SELECT blogs FROM Blogs blogs", Blogs.class); //hämtar blogs obj.
+			allBlogs = b.getResultList();
+			em.getTransaction();
+		} finally {
+			if(em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+			em.close();
+		}
+		return allBlogs;
+	}
+	
+	
+	
+	
+	
+	
+	
+/*
+	
+	
+	public Blogs removeBlog(Blogs blogID){
+		return blog;
+	} 
+	
+	
+	public List<Posts> listPosts(Blogs blogName){
+		return blog;
+	} 
+*/
+}
+
--- VemBloggen/src/se/vem/test/Test.java
+++ VemBloggen/src/se/vem/test/Test.java
@@ -2,25 +2,63 @@ package se.vem.test;
 
 import java.util.List;
 
+import se.vem.data.Blogs;
 import se.vem.data.User;
+import se.vem.databas.BlogsMapper;
 import se.vem.databas.UserMapper;
 
 public class Test {
 
 	public static void main(String[] args) {
+		addUser();
+		createBlog();
+		getBlog(5);
+		listAllBlogs();
+
+	}
+
+
+
+	private static void addUser() {
 		UserMapper userMapper = UserMapper.getInstance();
 		User user = new User();
 		user.setUsername("Robin");
 		user.setPassword("hemligt");
-		userMapper.registrera(user);
+		userMapper.register(user);
 		
-		List<User> users = userMapper.h�mtaAllaPersoner();
+		List<User> users = userMapper.listUsers();
 		int i = 1;
 		for (User listUser : users){
 			System.out.print(i++ + " ");
 			System.out.println(listUser);
 		}
+	}
+	// om adduser får id 10 så får bloggen id 11??
+	// nästa adduser får id 12 och bloggen 13.
+	private static void createBlog(){
+		BlogsMapper blogMapper = BlogsMapper.getInstance();
+		Blogs blog = new Blogs();
+		blog.setOwner_id(2);
+		blog.setTitle("Alpine");
+		blogMapper.createBlog(blog);
+	}
+	//hämtar ett blog obj med blog_id samt skriver ut titeln.
+	private static void getBlog(int i) {
+		BlogsMapper blogMapper = BlogsMapper.getInstance();
+		Blogs b = blogMapper.getBlog(i);
+		System.out.println(b.getTitle());
 		
+	}
+	
+	// lista alla bloggar
+	private static void listAllBlogs(){
+		BlogsMapper blogMapper = BlogsMapper.getInstance();
+			List<Blogs> blogs = blogMapper.listAllBlogs(); //vi får alla obj i db.
+			int i = 1;
+			for (Blogs listBlogs : blogs){
+				System.out.print(i++ + " ");
+				System.out.println(listBlogs.getTitle()); //Skriver ut titeln på bloggen.
+			}
 	}
 
 }
