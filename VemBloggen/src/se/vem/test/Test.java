8d30b2837c81b7c352c3763b5bed63e4e24d60ca
denhed <dennis.hedlund@me.com>
2013-09-10 14:34:29 +0200

    d

--- VemBloggen/src/se/vem/test/Test.java
+++ VemBloggen/src/se/vem/test/Test.java
@@ -3,25 +3,59 @@ package se.vem.test;
 import java.util.List;
 
 import se.vem.data.Blogs;
+import se.vem.data.Posts;
 import se.vem.data.User;
 import se.vem.databas.BlogsMapper;
+import se.vem.databas.PostsMapper;
 import se.vem.databas.UserMapper;
 
 public class Test {
 
 	public static void main(String[] args) {
-		addUser();
-		createBlog();
-		getBlog(5);
+		//addUser();
+		//createBlog();
+		getBlog(73);
 		listAllBlogs();
-		updateBlog();
+		//updateBlog();
+		//removeBlogs();
+		addPost("andra posten"," detta var en blog.");
 
 	}
+	
 
+	
+	private static void addPost(String titel, String text){
+		PostsMapper postsMapper = PostsMapper.getInstance();
+		Posts p = new Posts();
+		p.setTitle(titel);
+		p.setText(text);
+		postsMapper.createPost(p);
+	}
+	
+	private static void removeBlogs(){
+		BlogsMapper blogMapper = BlogsMapper.getInstance();
+		
+		long blog_id =  69; // det objekt vi ska radera.
+		int res = blogMapper.removeBlog(blog_id);
+		
+		if( res == 1) {
+			System.out.println("blog raderades");
+		}else if (res == 2){
+			System.out.println("inget raderades.");
+		}else if(res == 3){
+			System.out.println("hittade inget objekt att radera.");
+		}
+	}
+	
 	private static void updateBlog(){
 		BlogsMapper blogMapper = BlogsMapper.getInstance();
 		String title = "de twoow";
-		blogMapper.editBlog(11, title);
+		long blogId = 71;
+		if(blogMapper.editBlog(blogId, title) != null){
+			System.out.println("objekt uppdaterat");
+		}else{
+			System.out.println("hittade inget objekt");
+		}
 	}
 
 	private static void addUser() {
@@ -37,8 +71,7 @@ public class Test {
 			System.out.println(listUser);
 		}
 	}
-	// om adduser får id 10 så får bloggen id 11??
-	// nästa adduser får id 12 och bloggen 13.
+
 	private static void createBlog(){
 		BlogsMapper blogMapper = BlogsMapper.getInstance();
 		Blogs blog = new Blogs();
@@ -46,11 +79,17 @@ public class Test {
 		blog.setTitle("Alpine");
 		blogMapper.createBlog(blog);
 	}
+	
 	//hämtar ett blog obj med blog_id samt skriver ut titeln.
-	private static void getBlog(int i) {
+	private static void getBlog(long i) {
 		BlogsMapper blogMapper = BlogsMapper.getInstance();
 		Blogs b = blogMapper.getBlog(i);
-		System.out.println(b.getTitle());
+
+		if(b != null){
+			System.out.println(b.getTitle());
+		}else{
+			System.out.println("hittade inget blog objekt");
+		}
 		
 	}
 	
@@ -60,7 +99,7 @@ public class Test {
 			List<Blogs> blogs = blogMapper.listAllBlogs(); //vi får alla obj i db.
 			int i = 1;
 			for (Blogs listBlogs : blogs){
-				System.out.print(i++ + " ");
+				System.out.print(listBlogs.getBlog_id() + " "); //skriver ut blog_id.
 				System.out.println(listBlogs.getTitle()); //Skriver ut titeln på bloggen.
 			}
 	}
