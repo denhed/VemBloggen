1e05647e4493c6f141ba9ed5612efd0cdcefb6e6
Martin Davik <Davik93@hotmail.se>
2013-09-12 11:11:14 +0200

    Försöker få allt att funka

---
+++ src/se/vem/data/Comments.java
@@ -0,0 +1,56 @@
+package se.vem.data;
+
+import java.io.Serializable;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Comments
+ *
+ */
+
+@Entity
+public class Comments implements Serializable {
+
+	   
+	@Id @GeneratedValue
+	private long comments_id;
+	private String poster;
+	private String text;
+	private String notice;
+	
+	private static final long serialVersionUID = 1L;
+
+	public Comments() {
+		super();
+	}   
+	public long getComments_id() {
+		return this.comments_id;
+	}
+
+	public void setComments_id(long comments_id) {
+		this.comments_id = comments_id;
+	}   
+	public String getPoster() {
+		return this.poster;
+	}
+
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}   
+	public String getText() {
+		return this.text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}   
+	public String getNotice() {
+		return this.notice;
+	}
+
+	public void setNotice(String notice) {
+		this.notice = notice;
+	}
+   
+}
---
+++ src/se/vem/databas/PostsMapper.java
@@ -0,0 +1,105 @@
+package se.vem.databas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import se.vem.data.Posts;
+
+public class PostsMapper {
+	private static PostsMapper postsMapper = new PostsMapper();
+	private DatabaseConnection connection = null;
+	private static Logger logg = null;
+	
+	private PostsMapper(){
+		logg = Logger.getLogger(PostsMapper.class.getCanonicalName());
+		connection = DatabaseConnection.getInstance();
+		logg.info("Singeltobject created");
+	}
+	
+	public static PostsMapper getInstance(){
+		return postsMapper;
+	}
+	
+	public Posts createPost(Posts post){
+		EntityManager em = connection.getEntityManager();
+		try{
+			em.getTransaction().begin();
+			em.persist(post);
+			em.getTransaction().commit();
+		}finally{
+			if(em.getTransaction().isActive()){
+				em.getTransaction().rollback();
+			}
+				
+		}
+		em.close();
+		return post;
+	}
+	public Posts removePost(long posts_id){
+		EntityManager em = connection.getEntityManager();
+		Posts removePost;
+		
+		try{
+			em.getTransaction().begin();
+			removePost = em.find(Posts.class,posts_id);
+			em.remove(posts_id);
+			em.getTransaction().commit();
+		}finally{
+			if(em.getTransaction().isActive()){
+				em.getTransaction().rollback();
+			}
+			em.close();
+		}
+		return removePost;
+	}
+	
+	public List<Posts> getAllPosts(){
+		EntityManager em = connection.getEntityManager();
+		List<Posts> allPosts = null;
+		em.getTransaction().begin();
+		
+		try{
+			TypedQuery<Posts> p = em.createQuery("SELECT posts FROM Posts posts",Posts.class);
+			allPosts = p.getResultList();
+			em.getTransaction().commit();
+		}finally{
+			if(em.getTransaction().isActive()){
+				em.getTransaction().rollback();
+			}
+			em.close();	
+		}
+		if(allPosts == null)
+			allPosts = new ArrayList<Posts>();
+		return allPosts;
+	}
+	/**
+	 * Hämtar alla posts objekt med visst blog_id.
+	 * @return retunerar alla posts objekt.
+	 */
+	public List<Posts> getPostsWithBlogID(long blog_id){
+		
+		EntityManager em = connection.getEntityManager();
+		List<Posts> allPosts = new ArrayList<Posts>();
+		
+		em.getTransaction().begin();
+		
+		try {
+			TypedQuery<Posts> posts = em.createQuery("Select posts FROM Posts posts WHERE Posts.comments_id =:blogs_id", Posts.class); 
+			posts.setParameter("blogs_id", blog_id);
+			allPosts = posts.getResultList();
+			em.getTransaction();
+		} finally {
+			if(em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+			em.close();
+		}
+		return allPosts;
+	}
+
+}
+	
