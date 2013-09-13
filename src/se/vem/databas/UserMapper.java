6e0ac8d7056e0debf54ce9cabea5ae79b9e12063
Robin <laxen93a@gmail.com>
2013-09-14 01:05:10 +0200

    Fixade så att den kollar om användarnamnet redan existerar.

--- src/se/vem/databas/UserMapper.java
+++ src/se/vem/databas/UserMapper.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
 import se.vem.data.User;
@@ -28,43 +30,58 @@ public class UserMapper {
 
 	public User register(User user) {
 		EntityManager em = connection.getEntityManager();
+		String username = user.getUsername();
+
 		try {
 			em.getTransaction().begin();
 
-			em.persist(user);
+			// En query för att kolla om användaren existerar redan
+			Query q = em.createQuery(
+					"SELECT COUNT(user.username) FROM User user "
+							+ "WHERE User.username =: user", User.class);
+			q.setParameter("user", username);
+
+			// Här kollar vi om användarnamnet existerar eller inte i databasen
+			// Om det gör det, felmeddelande skrivs ut. Om det inte gör det
+			// registera då användaren.
+			Long result = (Long) q.getSingleResult();
+			if (result > 0) {
+				System.out.println("Användaren '" + username + "' finns redan!");
+			} else {
+				em.persist(user);
+			}
+
+			em.getTransaction().commit();
+
+		} finally {
+			if (em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+			em.close();
+		}
+		return user;
+	}
+
+	public User removeProfile(long user_id) {
+		EntityManager em = connection.getEntityManager();
+
+		User removeProfile = em.find(User.class, user_id);
+
+		try {
+			em.getTransaction().begin();
+
+			em.remove(removeProfile);
 
 			em.getTransaction().commit();
 		} finally {
 			if (em.getTransaction().isActive()) {
 				em.getTransaction().rollback();
 			}
-		}
-		em.close();
-
-		return user;
-
-	}
-
-	public User removeProfile(long user_id) {
-		EntityManager em = connection.getEntityManager();
-		
-		User removeProfile = em.find(User.class, user_id);
-		
-		try {
-			em.getTransaction().begin();
-			
-			em.remove(removeProfile);
-			
-			em.getTransaction().commit();
-		} finally {
-			if(em.getTransaction().isActive()) {
-				em.getTransaction().rollback();
-			}
 			em.close();
 		}
-		
+
 		return removeProfile;
-		
+
 	}
 
 	public User editProfile(long user_id, String username) {
